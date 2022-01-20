3c4ebba9399035e1295db2de2ebc54a228d71637
ismaelfari1996 <abel_fari@hotmail.com>
2022-01-19 20:32:16 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header';
 function App() {
   return (
     <Header />
-    
+  
   );
 }
 
--- src/components/body.js
+++ src/components/body.js
@@ -2,10 +2,10 @@ import Footer from "./footer";
 import Card from "./card";
 export default function body() {
     return (
-        <div className="container-lg">
+        <div className="container-fluid">
             <div className="row">
                 <h1 className="title center-text">
-                    <strong>Nuestros trabajos</strong>
+                    <strong>Nuestros Servicios</strong>
                 </h1>
             </div>
             <Card />
@@ -14,10 +14,21 @@ export default function body() {
                     <strong>Servicios</strong>
                 </h1>
             </div>
+            <Card />
             <div className="row">
                 <h1 className="title center-text">
                     <strong>Tecnologías con los que trabajamos</strong>
                 </h1>
+            </div>
+            <div className="row">
+                <div className="col col-lg-3 col-md-6 ">
+                    Back end
+                    <img className="img img-fluid" src="https://cdn.worldvectorlogo.com/logos/node-js-icon.svg" alt="node" />
+                </div>
+                <div className="col col-lg-3 col-md-6 ">
+                    Front end
+                    <img className="img img-fluid"  src="https://cdn.worldvectorlogo.com/logos/react-1.svg" alt="react" />
+                </div>
             </div>
             <div className="row">
                 <h1 className="title center-text">
