0a8a9a18f6e7c3ad7e6ee6fa482fc40b54c3792b
sysontec <johndoe@example.com>
2022-02-10 20:53:02 -0500

    commit

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -20,10 +20,10 @@ export default function Carousel() {
                         return (
                             <div key={index} className={item.active?"carousel-item h-inherit w-inherit w-100 active":"carousel-item h-inherit w-inherit w-100"} data-bs-interval="2000">
                                 <img src= {images(item.path)} className="d-block w-100" height={height} id="carousel-img" alt="..." />
-{/*                                <div className="carousel-caption d-none d-md-block">
-                                    <button type="button" className="btn btn-primary" >{item.title}</button>
+                                <div className="carousel-caption d-none d-md-block">
+                                    {/* <button type="button" className="btn btn-primary" >{item.title}</button> */}
                                     <p>{item.description}</p>
-                                </div>  */}
+                                </div>  
                             </div>
                         );
                     }
