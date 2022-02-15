6bf4b94922567f59c3b7323a92a0cefc4d3e8927
sysontec <johndoe@example.com>
2022-02-15 18:49:15 -0500

    add whatsapp

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,20 @@
+import icon from "../assets/img/icon/whatsapp.png"
 export default function Footer() {
     return (
-
+        <>
+        <a className="whatsapp"
+         href="https://api.whatsapp.com/send?phone=593984100360&text=Hola,%20¿Cómo%20podemos%20ayudarte?"
+         target={"_blank"}
+         rel="noreferrer noopener"
+         >
+        {/* <i className="fab fa-brands fa-whatsapp text-center"></i> */}
+        < img src={icon}></img>
+        </a>
         <footer className="text-center text-lg-start bg-light text-muted pt-3" id="contact">
-
-
             <section className="">
                 <div className="container text-center text-md-start mt-5">
-
                     <div className="row mt-3">
-
                         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
                             <h6 className="text-uppercase fw-bold mb-4">
                                 <i className="fas fa-gem me-3"></i>SySon TEC
                             </h6>
@@ -18,15 +22,12 @@ export default function Footer() {
                             Más vale tarde que nunca, digitallizate hoy, el futuro es tuyo. 
                             </p>
                         </div>
-
-
                         <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                         <h6 className="text-uppercase fw-bold mb-4">
                                         Tecnologías
                                     </h6>
                             <div className="row">
-                                <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-4">
-                                   
+                                <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-4"> 
                                     <p>
                                         <a href="#!" className="text-reset">PHP</a>
                                     </p>
@@ -39,10 +40,8 @@ export default function Footer() {
                                     <p>
                                         <a href="#!" className="text-reset">SQL</a>
                                     </p>
-
                                 </div>
                                 <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-4">
-
                                     <p>
                                         <a href="#!" className="text-reset">Python</a>
                                     </p>
@@ -57,13 +56,9 @@ export default function Footer() {
                                     </p>
                                 </div>
                             </div>
-
-
                         </div>
 
-
                         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
                             <h6 className="text-uppercase fw-bold mb-4">
                                 Contactos
                             </h6>
@@ -74,20 +69,15 @@ export default function Footer() {
                             </p>
                             <p><i className="fab fa-whatsapp"></i> +593 984100360</p>
                             <p><i className="fab fa-telegram-plane"></i> +593 999545982</p>
-
                         </div>
-
                     </div>
-
                 </div>
             </section>
-
             <div className="text-center p-4" style={{ "backgroundColor": "rgba(0, 0, 0, 0.05)" }}>
                 © 2022 SySon TEC
-
             </div>
-
         </footer>
+        </>
 
     )
 }
